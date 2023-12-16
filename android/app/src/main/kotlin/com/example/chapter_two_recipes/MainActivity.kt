9f732e5a84e460edd01430c3b74061e16b9f446f
eshad <fidaeshad10@gmail.com>
2023-12-16 06:37:10 -0600

    application complete

---
+++ android/app/src/main/kotlin/com/example/chapter_two_recipes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapter_two_recipes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/recipe.dart
@@ -0,0 +1,130 @@
+class Recipe {
+  String label;
+  String imageUrl;
+  int servings;
+  List<Ingredient> ingredients;
+
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
+
+  static List<Recipe> samples = [
+    Recipe(
+      'Spaghetti and Meatballs',
+      'assets/spaghetti.jpeg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          'Spaghetti',
+        ),
+        Ingredient(
+          4,
+          '',
+          'Frozen Meatballs',
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'sauce',
+        ),
+      ],
+    ),
+    Recipe(
+      'Tomato Soup',
+      'assets/tomato.jpg',
+      2,
+      [
+        Ingredient(
+          1,
+          'can',
+          'Tomato Soup',
+        ),
+      ],
+    ),
+    Recipe('Grilled Cheese', 'assets/cheese.jpeg', 2,
+        [Ingredient(4, 'Slice', 'Bread'), Ingredient(2, 'Slice', 'Cheese')]),
+    Recipe(
+      'Chocolate Chip Cookies',
+      'assets/cookie.jpeg',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
+    ),
+    Recipe(
+      'Taco Salad',
+      'assets/taco.jpeg',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'taco meat',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
+    ),
+    Recipe(
+      'Hawaiian Pizza',
+      'assets/pizza.jpeg',
+      4,
+      [
+        Ingredient(
+          1,
+          'item',
+          'pizza',
+        ),
+        Ingredient(
+          1,
+          'cup',
+          'pineapple',
+        ),
+        Ingredient(
+          8,
+          'oz',
+          'ham',
+        ),
+      ],
+    ),
+  ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
+}
